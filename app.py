8362f570aa9727f932e474a40c11c398d9c6b454
aryanbrite <aryanbrite@gmail.com>
2026-09-02 13:35:58 +0530

    added email

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import resend
+from resend.exceptions import ResendError
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+resend.api_key = os.environ["RESEND_API_KEY"]
+
+
+
+@app.route("/", methods = ["POST", "GET"])
 def home():
+    if request.method == "POST":
+        mail = request.form.get("email")
+        params: resend.Emails.SendParams = {
+            "from": "Aryan <i@aryan.my>",
+            "to": [mail],
+            "subject": "hello world",
+            "html": "<strong>it works!</strong>",
+        }
+
+        try:
+            email = resend.Emails.send(params)
+            return email
+        except ResendError as error:
+            return error
+        
     return render_template("index.html")
 
 
